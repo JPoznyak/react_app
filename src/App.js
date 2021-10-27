e1b23ece7ef7dccc0d816e31fed49519bd15a626
Jenya Poznyak <jendy.light@gmail.com>
2021-10-27 17:17:36 +0300

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Message } from "./components/Message/Message";
+import { Counter } from "./components/Counter/Counter";
+import "./App.scss";
 
 function App() {
+  const [text, setText] = useState("I am a GB student");
+
+  const handleClick = () => {
+    alert("click");
+    setText(Math.floor(Math.random()*100));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Message message={text} onMessageClick={handleClick} />
+        <p>My First React App</p>
+        <Counter />
       </header>
     </div>
   );

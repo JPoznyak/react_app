e59a66c423ff0860f0a91a430fe3c847be4ca8e4
Jenya Poznyak <jendy.light@gmail.com>
2021-11-19 11:30:37 +0300

    fix

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -38,45 +38,7 @@ export const ChatList = ({ chatList, addChat, deleteChat }) => {
             <ListGroup className="list" variant="flush">
                 {chatList.map((chat) => (
                     <ChatItem key={chat.id} chat={chat} deleteChat={deleteChat} />
-                   
-                // <>
-                // <ListGroup.Item className="group">
-                //     <NavLink
-                //         style={({ isActive }) => ({ color: isActive ? "#a83d3d" : "#5c4a4a" })}
-                //         to={`/chats/${chat.id}`}
-                //     >
-                //         {chat.name}
-                //     </NavLink>
-                //     <button className="del-btn" onClick={() => {}}>Delete</button>
-                //     {/* <Button type="submit" onClick={() => {}}>
-                //     Delete
-                //     </Button> */}
-                // </ListGroup.Item>
-                // </>
                 ))}
             </ListGroup>
-
-    
-
-
         </Container>
     )}
-
-    // return (
-    //     <div>
-    //       <h3>List of chats</h3>
-    //       <ul>
-    //         {chatList.map((chat) => (
-    //           <li key={chat.id}>
-    //             <ChatItem chat={chat} onDeleteChat={onDeleteChat} />
-    //           </li>
-    //         ))}
-    //       </ul>
-    //       <form onSubmit={handleSubmit}>
-    //         <TextField value={value} onChange={handleChange} />
-    //         <button>Add chat</button>
-    //       </form>
-    //     </div>
-    //   );
-
-
